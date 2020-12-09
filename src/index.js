0aaff0a032d8a31264fb581ee8c7df6e29c1d814
Fabian Krutsch <f.krutsch@gmx.de>
2020-12-09 21:52:08 +0000

    fix path logic

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+const fs = require("fs");
+const path = require("path");
+
 module.exports = ({ buildOptions }, { transformers }) => ({
   name: "snowpack-plugin-sharp",
-  async transform({ fileExt, id: filename, isDev, filePath }) {
+
+  async transform(x, { fileExt, id: filename, isDev } = x) {
     const transformer = transformers.find(
       (transformer) => transformer.fileExt === fileExt
     );
@@ -9,9 +13,27 @@ module.exports = ({ buildOptions }, { transformers }) => ({
       if (transformer) {
         if (transformer.withPreview) {
           // Create low quality image optionally
+
+          const inputFromSrc = filename.split(`${path.sep}src`).pop();
+          const inputArr = inputFromSrc.split(path.sep);
+          // Add preview Prefix
+          inputArr[inputArr.length - 1] = `preview-${
+            inputArr[inputArr.length - 1]
+          }`;
+
+          await fs.promises.mkdir(
+            `${buildOptions.out}${path.sep}_dist_${path.sep}${inputArr
+              .slice(0, inputArr.length - 1)
+              .join(path.sep)}`,
+            { recursive: true }
+          );
           await transformer
             .withPreview(filename)
-            .toFile(`${buildOptions.out}\\_dist_\\preview-${filePath}`);
+            .toFile(
+              `${buildOptions.out}${path.sep}_dist_${path.sep}${inputArr.join(
+                path.sep
+              )}`
+            );
         }
 
         return await transformer.apply(filename).toBuffer();
